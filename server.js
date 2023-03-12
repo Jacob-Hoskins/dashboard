bf890df59f429d797b06263872a55ee241b9c71f
Jacob Hoskins <45977627+Jacob-Hoskins@users.noreply.github.com>
2023-03-12 16:36:27 -0400

    Update server.js
    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 const server = app.listen(port, () => {
   console.log("Server running");
